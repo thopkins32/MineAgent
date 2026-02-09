24a534b438e8b8400a78b24c31655f69dacfc817
thomashopkins32 <thomashopkins000@gmail.com>
2026-02-09 16:02:21 -0500

    Cleanup tests to keep only minimal useful tests

--- forge/src/test/java/com/mineagent/DataBridgeTest.java
+++ forge/src/test/java/com/mineagent/DataBridgeTest.java
@@ -28,46 +28,6 @@ class DataBridgeTest {
   }
 
   @Test
-  void setNetworkHandler_storesHandler() {
-    dataBridge.setNetworkHandler(mockNetworkHandler);
-
-    // Verify handler is stored by checking sendObservation calls it
-    Observation obs = new Observation(10.0, new byte[] {1, 2, 3});
-    dataBridge.sendObservation(obs);
-
-    verify(mockNetworkHandler, times(1)).setLatest(obs.frame(), obs.reward());
-  }
-
-  @Test
-  void sendObservation_callsNetworkHandlerSetLatest_whenHandlerIsSet() {
-    dataBridge.setNetworkHandler(mockNetworkHandler);
-
-    byte[] frame = {1, 2, 3, 4, 5};
-    double reward = 42.5;
-    Observation obs = new Observation(reward, frame);
-
-    dataBridge.sendObservation(obs);
-
-    verify(mockNetworkHandler, times(1)).setLatest(frame, reward);
-  }
-
-  @Test
-  void sendObservation_logsWarning_whenHandlerIsNull() {
-    // Set handler to null by not calling setNetworkHandler or setting it to null
-    // Since we can't directly set to null, we'll test with a fresh instance scenario
-    // Actually, we need to test the case where handler is null
-    // Let's create a scenario where handler might be null
-    DataBridge freshBridge = DataBridge.getInstance();
-    // If handler was never set, it should be null
-    // But since it's a singleton, we can't easily reset it
-    // Instead, we'll verify the behavior when handler is null by not setting it
-
-    Observation obs = new Observation(10.0, new byte[] {1, 2, 3});
-    // This should not throw, but log a warning
-    assertDoesNotThrow(() -> freshBridge.sendObservation(obs));
-  }
-
-  @Test
   void setLatestRawInput_andGetLatestRawInput_getAndSetBehavior() {
     RawInput input1 = new RawInput(new int[] {65}, 1.0f, 2.0f, (byte) 1, 0.5f, "test");
     RawInput input2 = new RawInput(new int[] {66}, 3.0f, 4.0f, (byte) 2, 1.0f, "test2");
@@ -92,76 +52,6 @@ class DataBridgeTest {
   }
 
   @Test
-  void getLatestRawInput_returnsNull_whenNoInputSet() {
-    RawInput retrieved = dataBridge.getLatestRawInput();
-    assertNull(retrieved);
-  }
-
-  @Test
-  void getLatestRawInput_clearsInputAfterRetrieval() {
-    RawInput input = new RawInput(new int[] {65}, 1.0f, 2.0f, (byte) 1, 0.5f, "test");
-    dataBridge.setLatestRawInput(input);
-
-    RawInput first = dataBridge.getLatestRawInput();
-    assertNotNull(first);
-
-    RawInput second = dataBridge.getLatestRawInput();
-    assertNull(second);
-  }
-
-  @Test
-  void getInputInjector_returnsSameInstance() {
-    InputInjector injector1 = dataBridge.getInputInjector();
-    InputInjector injector2 = dataBridge.getInputInjector();
-
-    assertSame(injector1, injector2);
-  }
-
-  @Test
-  void setClientConnected_andIsClientConnected_stateManagement() {
-    // Initially should be false (or whatever default)
-    boolean initial = dataBridge.isClientConnected();
-
-    // Set to true
-    dataBridge.setClientConnected(true);
-    assertTrue(dataBridge.isClientConnected());
-
-    // Set to false
-    dataBridge.setClientConnected(false);
-    assertFalse(dataBridge.isClientConnected());
-
-    // Set to true again
-    dataBridge.setClientConnected(true);
-    assertTrue(dataBridge.isClientConnected());
-  }
-
-  @Test
-  void setClientConnected_tracksStateChanges() {
-    // Test multiple state changes
-    dataBridge.setClientConnected(true);
-    assertTrue(dataBridge.isClientConnected());
-
-    dataBridge.setClientConnected(false);
-    assertFalse(dataBridge.isClientConnected());
-
-    dataBridge.setClientConnected(true);
-    assertTrue(dataBridge.isClientConnected());
-
-    dataBridge.setClientConnected(false);
-    assertFalse(dataBridge.isClientConnected());
-  }
-
-  @Test
-  void setClientConnected_withSameValue_doesNotChangeState() {
-    dataBridge.setClientConnected(true);
-    assertTrue(dataBridge.isClientConnected());
-
-    // Set to same value
-    dataBridge.setClientConnected(true);
-    assertTrue(dataBridge.isClientConnected());
-  }
-
-  @Test
   void integration_setLatestRawInputThenGetAndSendObservation() {
     dataBridge.setNetworkHandler(mockNetworkHandler);
 
@@ -178,16 +68,5 @@ class DataBridgeTest {
     dataBridge.sendObservation(obs);
 
     verify(mockNetworkHandler, times(1)).setLatest(obs.frame(), obs.reward());
-  }
-
-  @Test
-  void getInputInjector_canBeUsedToInjectInput() {
-    InputInjector injector = dataBridge.getInputInjector();
-    assertNotNull(injector);
-
-    // Verify it's a functional injector
-    RawInput input = new RawInput(new int[] {}, 0.0f, 0.0f, (byte) 0, 0.0f, "");
-    // Should not throw (though it may log warnings if Minecraft not initialized)
-    assertDoesNotThrow(() -> injector.inject(input));
   }
 }
